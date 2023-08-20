746227342be298488d85adc5a61b1d477ff359e5
yessine <yessine.agrebi.aiesec@gmail.com>
2023-08-20 14:57:46 +0200

    fix marker

--- src/pages/map/MarkerMap.jsx
+++ src/pages/map/MarkerMap.jsx
@@ -1,177 +1,62 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Polyline } from "react-leaflet";
-import Leaflet from 'leaflet'
-import Api from "../../axios/Api";
-import FlatpickerPage from "./FlatpickerPage";
-import { Button, Card } from "reactstrap";
-import { getPosition, getStatus } from "../../service/DeviceService";
-import Statustable from "../../utils/Statustable";
+import Leaflet from 'leaflet';
 import socketIO from "socket.io-client";
-import iconMarker from "../../assets/images/svg/car-side.svg"
 
-export const newicon = new Leaflet.Icon({
-  iconUrl: iconMarker,
-  iconAnchor: [5, 55],
-  popupAnchor: [10, -44],
-  iconSize: [25, 55],
-})
 const MarkerMap = () => {
-  const [initialPosition, setInitialPosition] = useState([
-    34.754684, 10.755748,
-  ]);
+  const [initialPosition, setInitialPosition] = useState([34.754684, 10.755748]);
   const [positions, setPositions] = useState([]);
-  const [status, setStatus] = useState([]);
-  const [historyPositions, setHistoryPositions] = useState([]);
-  const markerRef = useRef(null);
   const [polylineCoord, setPolylineCoord] = useState([]);
   const [isVisible, setisVisible] = useState(false);
-  const handlButtonClick = () => {
-    setTimeout(() => {
-      setisVisible(!isVisible);
-    }, 500);
-  };
 
   const userData = JSON.parse(localStorage.getItem("userData"));
   const deviceId = userData.devices;
-  // const deviceIdList = userData?.cars?.map((car) => car.device_id) || [];
-  // const CarsAndDeviceStatus = async() => {
-  //   try {
-  //     const statusPromises = deviceIdList.map((deviceId) =>
-  //       getStatus(deviceId)
-  //         .then((response) => response)
-  //         .catch((error) => console.error(error.message))
-  //     );
-  //     const allStatus = await Promise.all(statusPromises);
 
-  //     // Combine car data and status data
-  //     const combinedData = userData?.cars?.map((car, index) => ({
-  //       carModel: car.model,
-  //       carNumSerie: car.num_serie,
-  //       batteryLevel: allStatus[index]?.telemetry?.["battery.level"]?.value,
-  //       defenseActiveStatus: allStatus[index]?.telemetry?.["defense.active.status"]?.value,
-  //       gsmSignalLevel: allStatus[index]?.telemetry?.["gsm.signal.level"]?.value,
-  //       // Add other properties from the status object as needed
-  //     }));
-
-  //     console.log(combinedData);
-  //     setStatus(combinedData);
-  //   } catch (error) {
-  //     console.error("Failed to fetch car status:", error.message);
-  //   }
-  // }
+  // Create a state to hold marker references for each device
+  const [markerRefs, setMarkerRefs] = useState({});
 
   useEffect(() => {
     const socket = socketIO(import.meta.env.VITE_URL);
 
-    // Listen for 'positionUpdate' event from the server
     const handlePositionUpdate = (data) => {
-      // Find the index of the position in the positions array based on the device ID
-      const deviceIndex = positions.findIndex(
-        (pos) => pos.deviceId === data.id
-      );
-
-      // Update the position or add a new position if not found
-      if (deviceIndex !== -1) {
-        const updatedPositions = [...positions];
-        updatedPositions[deviceIndex] = {
-          deviceId: data.id,
-          latitude: data.latitude,
-          longitude: data.longitude,
-        };
-        setPositions(updatedPositions);
-      } else {
-        setPositions((prevPositions) => [
-          ...prevPositions,
-          {
-            deviceId: data.id,
-            latitude: data.latitude,
-            longitude: data.longitude,
-          },
-        ]);
+      // Create or update the marker reference for the device
+      if (data.latitude !== null && data.longitude !== null) {
+        if (!markerRefs[data.id]) {
+          const newMarker = Leaflet.marker([data.latitude, data.longitude], {
+            // Add marker options if needed
+          });
+          setMarkerRefs((prevRefs) => ({
+            ...prevRefs,
+            [data.id]: newMarker,
+          }));
+        } else {
+          markerRefs[data.id].setLatLng([data.latitude, data.longitude]);
+        }
       }
     };
 
     socket.emit("getInitialPosition", deviceId);
     socket.on("positionUpdate", handlePositionUpdate);
 
-    // CarsAndDeviceStatus()
-    // Clean up the event listener when the component unmounts
     return () => {
       socket.off("positionUpdate", handlePositionUpdate);
       socket.disconnect();
     };
-  }, []);
+  }, [markerRefs, deviceId]);
 
   useEffect(() => {
-    // Update marker positions for all devices
-    positions.forEach((pos) => {
-      if (
-        pos.latitude !== null &&
-        pos.longitude !== null &&
-        markerRef.current[pos.deviceId]
-      ) {
-        markerRef.current[pos.deviceId].setLatLng([
-          pos.latitude,
-          pos.longitude,
-        ]);
-      }
-    });
+    // Update polyline coordinates
+    const updatedPolylineCoord = positions.map(({ latitude, longitude }) => [latitude, longitude]);
+    setPolylineCoord(updatedPolylineCoord);
   }, [positions]);
-
-  useEffect(() => {
-    if (historyPositions.length > 0) {
-      const updatedPolylineCoord = historyPositions.map(
-        ({ latitude, longitude }) => [latitude, longitude]
-      );
-      setPolylineCoord(updatedPolylineCoord);
-
-      const lastPosition =
-        updatedPolylineCoord[updatedPolylineCoord.length - 1];
-      markerRef.current.setLatLng(lastPosition);
-    }
-  }, [historyPositions]);
-
-  const coordinates = positions?.map(({ latitude, longitude }) => [
-    latitude,
-    longitude,
-  ]);
-
-  const handleDateRangeChange = async (startDate, endDate) => {
-    console.log("first:", startDate, "last:", endDate);
-    try {
-      let filteredPositions = [];
-      if (endDate) {
-        const response = await Api.get(
-          `/history/${deviceId}/${startDate}/${endDate}`
-        );
-        filteredPositions = response.data;
-      } else {
-        const response = await Api.get(`/history/${deviceId}/${startDate}`);
-        filteredPositions = response.data;
-      }
-
-      setHistoryPositions(filteredPositions);
-    } catch (error) {
-      console.error(error.message);
-    }
-  };
 
   return (
     <div className="w-full h-[400px]">
-      <Button
-        text="success"
-        className=" btn-outline-success rounded-[999px] m-1"
-        onClick={handlButtonClick}
-      >
-        Historique
-      </Button>
-      {isVisible && (
-        <FlatpickerPage onDateRangeChange={handleDateRangeChange} />
-      )}
+      {/* ... */}
       <MapContainer
         center={
-          coordinates.length > 0
-            ? coordinates[coordinates.length - 1]
+          positions.length > 0
+            ? [positions[positions.length - 1].latitude, positions[positions.length - 1].longitude]
             : initialPosition
         }
         zoom={7}
@@ -184,25 +69,18 @@ const MarkerMap = () => {
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        {/* Loop through positions and create markers for each device */}
-        {positions.map(
-          (pos, index) =>
-            pos.latitude !== null &&
-            pos.longitude !== null && (
-              <Marker
-                key={index}
-                ref={markerRef}
-                position={[pos.latitude, pos.longitude]}
-                interactive={false}
-                icon={newicon}
-              />
-            )
-        )}
+        {Object.keys(markerRefs).map((deviceId) => (
+          <Marker
+            key={deviceId}
+            position={markerRefs[deviceId].getLatLng()}
+            ref={markerRefs[deviceId]}
+            interactive={false}
+          />
+        ))}
         {polylineCoord.length > 0 && (
           <Polyline positions={polylineCoord} color="red" smoothFactor={0.5} />
         )}
       </MapContainer>
-      {/* <Statustable status={status} /> */}
     </div>
   );
 };
